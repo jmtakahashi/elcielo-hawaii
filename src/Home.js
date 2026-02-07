688f25dd360494e3aa010d66df175ce51ec7ef8f
Jason Takahashi <jmtakahashi@gmail.com>
2026-02-07 15:38:52 -0800

    refactor home page html markup for semantic accuracy.

--- src/Home.js
+++ src/Home.js
@@ -2,35 +2,40 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <section className='home'>
-      <section className='splash'>
-        <p className='splash-text'>
-          <span>A culinary journey that</span>
-          <span>found its meaning in Hawaii</span>
-        </p>
-        <button>Reservations</button>
+    <>
+      <section className='hero'>
+        <div className='hero-background'></div>
+        <div className='hero-content'>
+          <p className='hero-text'>
+            <span>A culinary journey that</span>
+            <span>found its meaning in Hawaii</span>
+          </p>
+          <button>Reservations</button>
+        </div>
       </section>
 
       <section className='story'>
-        <h3>Born in argentina</h3>
+        <span>Born in argentina</span>
         <span className='story-divider'></span>
-        <h3>Shaped in Japan</h3>
+        <span>Shaped in Japan</span>
         <span className='story-divider'></span>
-        <h3>Refined in Spain</h3>
+        <span>Refined in Spain</span>
         <span className='story-divider'></span>
-        <h3>Perfected in Los Angeles</h3>
+        <span>Perfected in Los Angeles</span>
         <span className='story-divider'></span>
       </section>
 
       <section className='definition'>
         <span className='story-divider'></span>
-        <span>El Cielo is</span>
-        <span>a quiet expression of</span>
-        <span>movement, memory and place</span>
-        <span>where language</span>
-        <span>becomes unnecessary</span>
-        <span>at the table.</span>
+        <p>
+          <span >El Cielo is</span>
+          <span className='large'>a quiet expression of</span>
+          <span className='large'>movement, memory and place</span>
+          <span >where language</span>
+          <span >becomes unnecessary</span>
+          <span >at the table.</span>
+        </p>
       </section>
-    </section>
+    </>
   );
 }
