4e52a0ce1242fc8e489a31866f8d38e742b6fd21
Jason Takahashi <jmtakahashi@gmail.com>
2026-02-07 15:38:08 -0800

    refactor html markup to add mobile menu.

--- src/Header.js
+++ src/Header.js
@@ -6,10 +6,14 @@ import { faFacebook, faInstagram, faYelp, faGoogle } from '@fortawesome/free-bra
 export default function Header() {
   return (
     <header className='header'>
-      <h1>El Cielo Hawaii</h1>
-      <img src='/assets/logo.png' alt='El Cielo Hawaii Logo' className='logo' />
+      <h1 hidden>El Cielo Hawaii</h1>
+      <img
+        src='images/logo-black.png'
+        alt='El Cielo Hawaii Logo'
+        className='logo'
+      />
 
-      <nav className='nav'>
+      <nav className='nav desktop'>
         <NavLink
           to='/'
           className={({ isActive, isPending }) =>
@@ -43,7 +47,7 @@ export default function Header() {
         </a>
       </nav>
 
-      <div className='socials'>
+      <div className='socials desktop'>
         <a
           href='https://www.facebook.com/people/El-Cielo-by-Masa-Gushiken/61557016725011/#'
           target='_blank'
@@ -72,6 +76,73 @@ export default function Header() {
         >
           <FontAwesomeIcon icon={faGoogle} />
         </a>
+      </div>
+
+      <div className='mobileMenu'>
+        <nav className='nav'>
+          <NavLink
+            to='/'
+            className={({ isActive, isPending }) =>
+              isPending ? 'pending' : isActive ? 'active' : ''
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to='/about'
+            className={({ isActive, isPending }) =>
+              isPending ? 'pending' : isActive ? 'active' : ''
+            }
+          >
+            About
+          </NavLink>
+          <NavLink
+            to='/menu'
+            className={({ isActive, isPending }) =>
+              isPending ? 'pending' : isActive ? 'active' : ''
+            }
+          >
+            Menu
+          </NavLink>
+          <a
+            href='https://www.opentable.com/booking/restref/availability?lang=en-US&correlationId=49e3accf-db76-4750-b873-65114256b1d0&restRef=1345438&otSource=Restaurant%20website'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Reservations
+          </a>
+        </nav>
+
+        <div className='socials'>
+          <a
+            href='https://www.facebook.com/people/El-Cielo-by-Masa-Gushiken/61557016725011/#'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
+          <a
+            href='https://www.instagram.com/elcielo_hawaii'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+          <a
+            href='https://www.yelp.com/biz/el-cielo-by-chef-masa-honolulu-4'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FontAwesomeIcon icon={faYelp} />
+          </a>
+          <a
+            href='https://www.google.com/search?q=el+cielo+by+chef+masa+reviews&oq=El+Cielo+by+chef+Masa&gs_lcrp=EgZjaHJvbWUqBwgBEAAYgAQyBwgAEAAYgAQyBwgBEAAYgAQyCAgCEAAYFhgeMggIAxAAGBYYHjIICAQQABgWGB4yCAgFEAAYFhgeMggIBhAAGBYYHjIICAcQABgWGB7SAQk0ODc2ajBqMTWoAgCwAgA&sourceid=chrome&ie=UTF-8#ip=1&lrd=0x7c006d9006881d5f:0x2777fc6aa28cc28a,1,,,,'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FontAwesomeIcon icon={faGoogle} />
+          </a>
+        </div>
       </div>
     </header>
   );
