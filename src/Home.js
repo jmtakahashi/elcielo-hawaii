8f7278059f058529df8cf199297bac71016f7c54
Jason Takahashi <jmtakahashi@gmail.com>
2026-02-16 23:21:14 -0800

    remove scroll event listener to test stutter.

--- src/Home.js
+++ src/Home.js
@@ -8,12 +8,12 @@ export default function Home({ headerEl }) {
   // fade into view on scroll
   useEffect(() => {
     const callback = (entries, observer) => {
-      // let delay = 0;
+      let delay = 0;
 
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          // entry.target.style.transitionDelay = `${delay}ms`;
-          // delay += 80;
+          entry.target.style.transitionDelay = `${delay}ms`;
+          delay += 80;
           entry.target.classList.add('revealed');
           observer.unobserve(entry.target); // unobserve after revealing
         }
@@ -36,27 +36,27 @@ export default function Home({ headerEl }) {
   }, []);
 
   // header transparency on scroll
-  useEffect(() => {
-    const observedSection = storySectionRef.current;
+  // useEffect(() => {
+  //   const observedSection = storySectionRef.current;
 
-    const handleScroll = (e) => {
-      if (!observedSection || !headerEl) return;
+  //   const handleScroll = (e) => {
+  //     if (!observedSection || !headerEl) return;
 
-      const observedSectionTop = observedSection.getBoundingClientRect().top;
+  //     const observedSectionTop = observedSection.getBoundingClientRect().top;
 
-      if (observedSectionTop < headerHeight) {
-        headerEl.classList.add('scrolledPastHero');
-      } else {
-        headerEl.classList.remove('scrolledPastHero');
-      }
-    };
+  //     if (observedSectionTop < headerHeight) {
+  //       headerEl.classList.add('scrolledPastHero');
+  //     } else {
+  //       headerEl.classList.remove('scrolledPastHero');
+  //     }
+  //   };
 
-    document.addEventListener('scroll', handleScroll);
+  //   document.addEventListener('scroll', handleScroll);
 
-    return () => {
-      document.removeEventListener('scroll', handleScroll);
-    };
-  }, [headerHeight, headerEl]);
+  //   return () => {
+  //     document.removeEventListener('scroll', handleScroll);
+  //   };
+  // }, [headerHeight, headerEl]);
 
   return (
     <div id='home'>
